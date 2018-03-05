1eab84310addc29846fb687aed0daf3de87e85df
erv4gen <ervik993@mail.com>
2018-03-04 21:33:19 -0600

    init commit

--- Post.py
+++ Post.py
@@ -1,73 +1,120 @@
+import traceback
+
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+import urllib
 from urllib.error import URLError
+
+import time
+import random
 import re
 from datetime import datetime, timedelta
 
-class Post(object) :
-	
-	def __init__(self, post_id, location, category, title='', content='', email='', date='', status = 0):
+from selenium.common.exceptions import NoSuchElementException
 
-		self.post_id = post_id
-		self.url = ('https://{}.craigslist.org/{}/{}.html').format(location, category, post_id)
-		self.location = location
-		self.category = category
-		self.title = title
-		self.content = content
-		self.email = email
-		self.date = date
-		self.status = status
 
-		self.get_post()
-		
-	def get_post(self):
+class Post(object):
 
-		# print(self.url)
-		try:
-			soup = BeautifulSoup(urlopen(self.url).read()).text
-			self.title = soup.title.string
+    def __init__(self, url, location, category, title='', content='', email='', date='', status=0, driver=''):
 
-			#get the content of the post
-			post_content = str(soup.find(id='postingbody')) #section', {'id':'postingbody'}).text)
+        self.url = url
+        self.location = location
+        self.category = category
+        self.title = title
+        self.content = content
+        self.email = email
+        self.date = date
+        self.status = status
+        self.driver = driver
+        self.get_post()
 
-			#remove repetions of <br> and [/br] at the end of the post
-			post_content = re.sub(r'(<br>){2,}','', post_content, flags=re.IGNORECASE)
-			post_content = re.sub(r'(</br>){2,}','', post_content, flags=re.IGNORECASE)
-			self.content = post_content
 
-			#get the reply email for the post, the info that I need are in another html page
-			reply = soup.find(id='replylink')#'a', {'id':'replylink'})
-			if reply is not None :
-				reply_link = reply['href']
-				reply_url = self.url[:self.url.find('.org/') + 4] + reply_link 
-				reply_page = BeautifulSoup(urlopen(reply_url).read())
-				self.email = reply_page.find('div', class_='anonemail').string # {'class':'anonemail'}).string 
-				# self.email = format(reply_email)
+    def get_post(self):
 
-		except URLError as error:
-			# print(error)
-			pass
+        # print(self.url)
+        try:
 
-	def get_location(self):
-		return self.location
+            def get_h():
+                headers = {
 
-	def get_category(self):
-		return self.category
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'}
+                return  headers
 
-	def get_catefory_full_name(self):
-		return self.category_full_name
+            req = urllib.request.Request(
+                self.url,
+                data=None,
+                headers= get_h()
+            )
 
-	def get_title(self):
-		return self.title
+            soup = BeautifulSoup(urllib.request.urlopen(req).read().decode('utf-8'), "html5lib")
+            time.sleep(10)
 
-	def get_body(self):
-		return self.content
 
-	def get_email(self):
-		return self.email
 
-	def get_date(self):
-		return self.date
+            self.title = soup.title.string
 
-	def __str__(self):
-		return 'Title:{title}. Date:{date}. Email:{email}. Location:{location}. Category:{category}'.format(title= self.title, date= self.date, email= self.email, location= self.location, category= self.category)
+            # get the content of the post
+            post_content = soup.find('section', {'id':'postingbody'})  # section', {'id':'postingbody'}).text)
+            # remove repetions of <br> and [/br] at the end of the post
+            #post_content = re.sub(r'(<br>){2,}', '', post_content, flags=re.IGNORECASE)
+            #post_content = re.sub(r'(</br>){2,}', '', post_content, flags=re.IGNORECASE)
+            self.content = post_content.text
+
+            # get the reply email for the post, the info that I need are in another html page
+            reply = soup.find(id='replylink')  # 'a', {'id':'replylink'})
+            if reply is not None:
+                reply_link = reply['href']
+                reply_url = self.url[:self.url.find('.org/') + 4] + reply_link
+
+                #reply_page = BeautifulSoup(urlopen(reply_url).read(),"html5lib")
+                #headers = get_h()
+                #for key, value in enumerate(headers):
+                  #  capability_key = 'phantomjs.page.customHeaders.{}'.format(key)
+                  #  webdriver.DesiredCapabilities.PHANTOMJS[capability_key] = value
+
+                p_element = None
+                try:
+                    time.sleep(10)
+                    self.driver.get(reply_url)
+                    p_element = self.driver.find_element_by_class_name('anonemail')
+
+                except NoSuchElementException as e:
+                    print("Fail with Capcha\n",e)
+                except Exception as e:
+                    print(e)
+                #reply_page = BeautifulSoup(urlopen(reply_url).read(), "html5lib")
+                #self.email = reply_page.find('div', class_='anonemail')#.string  # {'class':'anonemail'}).string
+                if p_element is not None:
+                    self.email = p_element.text
+                    print("Post", self.url, " saved successfully")
+                else:
+                    self.email = '-'
+            # self.email = format(reply_email)
+
+        except URLError as error:
+            print(error)
+        except Exception as e:
+            print(e)
+    def get_location(self):
+        return self.location
+
+    def get_category(self):
+        return self.category
+
+    def get_catefory_full_name(self):
+        return self.category_full_name
+
+    def get_title(self):
+        return self.title
+
+    def get_body(self):
+        return self.content
+
+    def get_email(self):
+        return self.email
+
+    def get_date(self):
+        return self.date
+
+    def __str__(self):
+        return 'Title:{title}. Date:{date}. Email:{email}. Location:{location}. Category:{category}'.format(
+            title=self.title, date=self.date, email=self.email, location=self.location, category=self.category)
--- scraper.py
+++ scraper.py
@@ -1,74 +1,123 @@
+import pandas as pd
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+import urllib.request
 from urllib.error import URLError
 import re
 from datetime import datetime, timedelta
 from Post import Post
+import time
+import random
+from selenium import webdriver
 
 def get_globals():
+    try:
+        config = {}
+        exec(open('config.conf').read(), config)
+        locations = config['locations']
+        categories = config['categories']
+        # output = '{}.html'.format(datetime.now().strftime('%Y%m%d%H%M'))
+        return (locations, categories)
 
-	try:
-		config = {}
-		exec(open('config.conf').read(), config)
-		locations = config['locations']
-		categories = config['categories']
-		# output = '{}.html'.format(datetime.now().strftime('%Y%m%d%H%M'))
-		return (locations, categories)
+    except Exception as e:
+        raise
 
-	except Exception as e:
-		raise
 
 def filter_by_date(ad_date):
-	# I just care about the new posts of today.
-	return datetime.now().date() == datetime.strptime(ad_date, '%Y-%m-%d %H:%M').date()
+    # I just care about the new posts of today.
+    return datetime.now().date() == datetime.strptime(ad_date, '%Y-%m-%d %H:%M').date()
+
+
+def get_proxy():
+    plist = ['50.234.205.212:80']
+    return plist
+
 
 def get_posts(location, category):
+    posts = []
 
-	posts = []
+    def get_h():
+        headers = {
 
-	url = ('https://' + location + '.craigslist.org/search/' + category)
-	try:
-		soup = BeautifulSoup(urlopen(url).read())
-	except URLError :
-		raise
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'}
+        return headers
 
-	#gets list of post ids
-	ids = soup.find_all('p', class_= 'row')
 
-	#remove nearby posts and old ones
-	ids = (post for post in ids if filter_by_date(post.time['datetime']))
-	ids = (post for post in ids if post.a['href'][0]=='/')
+    url = ('https://' + location + '.craigslist.org/search/' + category)
+    try:
+        #proxy_support = urllib.request.ProxyHandler({'http': get_proxy()})
+        #opener = urllib.request.build_opener(proxy_support)
+        #urllib.request.install_opener(opener)
 
-	for post in ids:
-		posts.append(Post(post['data-pid'], location, category, date = post.time['datetime']))
+        req = urllib.request.Request(
+            url,
+            data=None,
+            headers= get_h()
+                                )
 
-	return posts 
+        soup = BeautifulSoup(urllib.request.urlopen(req).read().decode('utf-8'), "html5lib")
+        time.sleep(10)
+
+    except URLError:
+        print()
+        raise
+
+    # gets list of post ids
+    ids = soup.find_all('li', class_='result-row')
+
+    # remove nearby posts and old ones
+    ids = [post for post in ids if filter_by_date(post.time['datetime'])]
+    #ids = [post for post in ids if post.a['href'][0] == '/']
+    #ids = [post.a['href'] for post in ids]
+    print("Next posts been found:")
+    driver = webdriver.Firefox(executable_path='c:\\Users\\15764\\Documents\\geckodriver\\geckodriver.exe')
+    for post in ids:
+       # print(post.a['href'])
+        posts.append(Post(post.a['href'], location, category, date=post.time['datetime'],driver=driver))
+
+    return posts
+
 
 def make_html(locations, categories, posts):
-	from jinja2 import Environment, PackageLoader
-	env = Environment(loader=PackageLoader('scraper', 'template'))
-	template = env.get_template('template.html')
-	html_output = template.render(locations= locations, categories= categories, posts= posts)
-	output_destination = '{}.html'.format(datetime.now().strftime('%Y%m%d%H%M'))
+    from jinja2 import Environment, PackageLoader
+    env = Environment(loader=PackageLoader('scraper', 'template'))
+    template = env.get_template('template.html')
+    html_output = template.render(locations=locations, categories=categories, posts=posts)
+    output_destination = '{}.html'.format(datetime.now().strftime('%Y%m%d_%H:%M'))
 
-	with open(output_destination,'wb') as output:
-		output.write(bytes(html_output, 'UTF-8'))
+    with open(output_destination, 'wb') as output:
+        output.write(bytes(html_output, 'UTF-8'))
 
-	
+def get_columns():
+    col = ['url','title','content','category','location','email','date']
+    return col
+
+def save_to_csv(posts):
+
+    df = pd.DataFrame(columns=get_columns())
+    for post in posts:
+        new_row = [post.url,post.title,post.content,post.category,post.location,post.email,post.date]
+        df.append(pd.Series(new_row),ignore_index=True)
+    today = datetime.now().strftime("%d_%m_%y") +".csv"
+    try:
+        df.to_csv(today, index=False)
+    except:
+        pass
 def main():
+    locations, categories = get_globals()
+    posts = []
 
-	locations, categories = get_globals()
-	posts = []
+    for location in locations:
+        for category in categories:
+            posts.extend(get_posts(location, category))
 
-	for location in locations :
-		for category in categories :
-			posts.extend(get_posts(location, category)) 
+        # make ouput
+    save_to_csv(posts)
+    make_html(locations, categories, posts)
+    print('Posts found:{n_posts}. Done!'.format(n_posts=len(posts)))
 
-	#make ouput
-	make_html(locations,categories,posts)
-	print('Posts found:{n_posts}. Done!'.format(n_posts=len(posts)))
 
 if __name__ == '__main__':
-	main()
-	# p = Post('51', 'asheville', 'sad')
-	# print(p)
+    print("Starting working..")
+    main()
+# p = Post('51', 'asheville', 'sad')
+# print(p)
